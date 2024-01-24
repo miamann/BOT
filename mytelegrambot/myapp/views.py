bd9a018e691b2795cc59327395cf9004ad544f62
крис <106028340+miamann@users.noreply.github.com>
2024-01-24 23:49:13 +0600

    views.py

--- mytelegrambot/myapp/views.py
+++ mytelegrambot/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from .telegram_bot import run_telegram_bot  # Обратите внимание на корректный путь
+from .telegram_bot import run_telegram_bot
 
 def run_bot(request):
     run_telegram_bot()
